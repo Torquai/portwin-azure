1ad6889320ded012c7a4aabb38a314ca78f5b417
Thomas Leggett <thom78@gmail.com>
2017-03-24 14:37:04 +0100

    qwerty

--- Portwin/deployPortwin.ps1
+++ Portwin/deployPortwin.ps1
@@ -6,9 +6,14 @@ $resourceGroupLocation = "North Europe"
 #Resourcegroup prefix to generate new resourcegroups
 $resourceGroupPrefix = "thomas-devtest"
 
-#Parameters to use as base for environment
-#Valid names: dev
-$baseEnvironment = "dev"
+$parameters = @{
+    "environment" = "dev";
+    "sqlAdminLoginName" = "sqladmin";
+    "sqlAdminPassword" = "a!r(Fc#&blah"
+
+    "storageSkuName" = "Standard_LRS";
+    "storageSkuTier" = "Standard";
+};
 
 #Url for parameters
 $baseParameterUrl = "https://github.com/Torquai/portwin-azure/raw/master/Portwin/parameters/"
@@ -16,19 +21,7 @@ $baseParameterUrl = "https://github.com/Torquai/portwin-azure/raw/master/Portwin
 #Url for templates
 $baseTemplateUrl = "https://github.com/Torquai/portwin-azure/raw/master/Portwin/templates/"
 
-$resourceGroupAuxName = $resourceGroupPrefix + "-aux"
-$resourceGroupInsightsName = $resourceGroupPrefix + "-insights"
-$resourceGroupStorageName = $resourceGroupPrefix + "-storage"
-$resourceGroupWebName = $resourceGroupPrefix + "-web"
 
-$parameterInsightsUrl = $baseParameterUrl + $baseEnvironment + ".insights.json"
-$parameterStorageUrl = $baseParameterUrl + $baseEnvironment + ".storage.json"
-$parameterServiceplanUrl = $baseParameterUrl + $baseEnvironment + ".serviceplan.json"
-
-$templateInsightsUrl = $baseTemplateUrl + "templateInsights.json"
-$templateStorageUrl = $baseTemplateUrl + "templateStorage.json"
-$templateServiceplanUrl = $baseTemplateUrl + "templateServiceplan.json"
-$templateFrontendUrl = $baseTemplateUrl + "templateFrontend.json"
 
 # sign in
 Write-Host "Logging in...";
@@ -38,23 +31,32 @@ Login-AzureRmAccount;
 Write-Host "Selecting subscription '$subscriptionId'";
 Select-AzureRmSubscription -SubscriptionID $subscriptionId;
 
-# Start the deployment
+# Start deployment of application insights
+$resourceGroupInsightsName = $resourceGroupPrefix + "-insights"
+$templateUri = $baseTemplateUrl + "templateInsights.json"
+
 Write-Host "Deploying application insights to resourcegroup '$resourceGroupInsightsName'...";
-New-AzureRmResourceGroup -Name $resourceGroupInsightsName -Location $resourceGroupLocation -Force -ErrorAction SilentlyContinue
-New-AzureRmResourceGroupDeployment `
-    -Mode Incremental `
-    -ResourceGroupName $resourceGroupInsightsName `
-    -TemplateUri $templateInsightsUrl `
-    -TemplateParameterObject @{ environment=$baseEnvironment } `
-    -Verbose
+#New-AzureRmResourceGroup -Name $resourceGroupInsightsName -Location $resourceGroupLocation -Force -ErrorAction SilentlyContinue
+#New-AzureRmResourceGroupDeployment `
+#    -Mode Incremental `
+#    -ResourceGroupName $resourceGroupInsightsName `
+#    -TemplateUri $templateUri `
+#    -TemplateParameterObject @{ environment=$baseEnvironment; } `
+#    -Verbose;
 Write-Host "Finished deployment of application insights"
+
+
+# Start deployment of storage
+$resourceGroupStorageName = $resourceGroupPrefix + "-storage"
+$templateUri = $baseTemplateUrl + "templateStorage.json"
+$storageParameters = @{ "environment" = $parameters.Get_Item("environment") };
 
 Write-Host "Deploying storage to resourcegroup '$resourceGroupStorageName'...";
 New-AzureRmResourceGroup -Name $resourceGroupStorageName -Location $resourceGroupLocation -Force -ErrorAction SilentlyContinue
 New-AzureRmResourceGroupDeployment `
     -Mode Incremental `
     -ResourceGroupName $resourceGroupStorageName `
-    -TemplateUri $templateStorageUrl `
-    -TemplateParameterObject @{ environment=$baseEnvironment; parameteruri=$parameterStorageUrl } `
-    -Verbose
+    -TemplateUri $templateUri `
+    -TemplateParameterObject $storageParameters `
+    -Verbose;
 Write-Host "Finished deployment of storage resources"
